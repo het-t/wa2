02d824d32294e3c84aca721e6c8a7cf45794b269
het-t <hettarkhala068@gmail.com>
2022-04-18 22:45:32 +0530

    1.0.0

--- rec.js
+++ rec.js
@@ -1,45 +1,10 @@
 var pdf = require('html-pdf');
 var con = require('./db.js').connection;
-var cron = require('cron').CronJob;
-var {Client, MessageMedia} = require('whatsapp-web.js');
-let sessionData;
-
-var fs = require('fs');
-const { resolve } = require('path');
-const SESSION_FILE_PATH = './session.json';
-var qrcode = require('qrcode-terminal');
-const { job } = require('cron');
-
-
-if(fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
-}
-
-var client =  new Client({
-    session: sessionData
-})
-
-client.on('qr', (qr) => {
-    qrcode.generate(qr,{small:true})
-    console.log('QR RECEIVED', qr);
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('authenticated', (session) => {
-    sessionData = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-});
-
+var sendPdf = require('./sendRec.js').sendPdf;
 var htmlAbove = `<!DOCTYPE html><html lang="en"><head><link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet"><style>th{padding-bottom:8px;}table thead th { border-bottom: 1px solid #c2c2c2; } table {border-collapse:collapse;width:80%;font-family: 'Lato', sans-serif;font-size:24px;text-align:right;margin-left:auto;margin-right:auto;} th, td {text-align:right;} td{color:#5c5c5c;font-weight:400;font-size:18px;} th {font-weight:bold;color:#4a4a4a;margin-bottom:12px;}tbody+tr>td {line-height:4px;margin-top:12px;}</style><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>report</title></head><body><table><thead><tr><th>Date</th><th>Description</th><th>Progress</th></tr></thead>`
 var htmlBelow = `</tbody></table></body></html>`
+const { job } = require('cron');
 
 var date = new Date().getDate();
 var userIdArray = [];
@@ -106,18 +71,8 @@ var getAllUsers = ()=>{
     })
 }
 
-var p = async () => {
-    await clientini()
-    pdfArray.forEach( (pdfelem)=>{
-        var media = MessageMedia.fromFilePath(pdfelem)
-        var user = pdfelem.substr(2,16)
-        user = user.replace('c','c.')
-    if (user== '919879034832@c.us') client.sendMessage('919879034832@c.us',media)
-    })
-} 
-var clientini = async ()=> {
-    await client.initialize()
-}
+
+
 var u = getAllUsers();
 
 u.then(async (user_id_array)=>{
@@ -128,13 +83,19 @@ u.then(async (user_id_array)=>{
     })
 })
 
-
-setTimeout(() => {
-    p()
-}, 600);
-
-// var shecudleSending = new job('*/10 * * * * *',()=>{
-//     console.log('------job----------')
-//     p()
-// })
-// shecudleSending.start();
+var shecudleSending = new job('30 22 * * * ',()=>{
+    try {
+        u.then(async (user_id_array)=>{
+            await user_id_array.forEach((uidObj)=>{
+                getProgressData(uidObj.user).then((obj)=>{
+                    createReport(obj.res,obj.uid)
+                })
+            })
+        })
+    }
+    finally{
+        sendPdf()
+    }
+    
+})
+shecudleSending.start();
